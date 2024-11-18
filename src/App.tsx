8ef4939c9919e78a8e1297cd523837880615f10f
s1_1v <s1_1v@naver.com>
2024-11-18 12:47:32 +0900

    🏷️ : [CD-61] 게임 state 추가

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,13 @@ import { Canvas } from '@react-three/fiber';
 import Scene from './components/Scene';
 import { KeyboardControls } from '@react-three/drei';
 import SocketController from './components/controller/SocketController';
+import { gameScreenAtom } from './atoms/GameAtoms';
+import { GameScreen } from './types/game';
+import { useAtom } from 'jotai';
+import LoadingPage from './components/pages/LoadingPage';
+import LoginPage from './components/pages/LoginPage';
+import HomePage from './components/pages/HomePage';
+import MatchingPage from './components/pages/MatchingPage';
 
 const keyboardMap = [
   { name: 'forward', keys: ['ArrowUp', 'KeyW'] },
@@ -13,19 +20,27 @@ const keyboardMap = [
 ];
 
 function App() {
+  const [gameScreen] = useAtom(gameScreenAtom);
+
   return (
     <KeyboardControls map={keyboardMap}>
-      <SocketController />
-      <Canvas
-        shadows
-        camera={{ position: [3, 3, 3], near: 0.1, fov: 60 }}
-        style={{ touchAction: 'none' }} // 브라우저 기본 동작 방지
-      >
-        <color attach="background" args={['skyblue']} />
-        <Scene />
-      </Canvas>
+      <SocketController>
+        {gameScreen === GameScreen.LOADING && <LoadingPage />}
+        {gameScreen === GameScreen.LOGIN && <LoginPage />}
+        {gameScreen === GameScreen.HOME && <HomePage />}
+        {gameScreen === GameScreen.MATCHING && <MatchingPage />}
+        {gameScreen === GameScreen.GAME && (
+          <Canvas
+            shadows
+            camera={{ position: [3, 3, 3], near: 0.1, fov: 60 }}
+            style={{ touchAction: 'none' }}
+          >
+            <color attach="background" args={['skyblue']} />
+            <Scene />
+          </Canvas>
+        )}
+      </SocketController>
     </KeyboardControls>
   );
 }
-
 export default App;
