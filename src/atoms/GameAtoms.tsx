e276c81b4af88431c06aa2fed3af45c26487ca36
Soojin Lee <asyncwaiter@gmail.com>
2024-11-20 15:01:35 +0900

    🐛: 서버 남은 시간 반영

--- src/atoms/GameAtoms.tsx
+++ src/atoms/GameAtoms.tsx
@@ -6,3 +6,4 @@ export const gameScreenAtom = atom<GameScreen>(GameScreen.LOADING);
 export const nicknameAtom = atom<string>('');
 
 export const socketServiceAtom = atom<SocketService | null>(null);
+export const gameTimeAtom = atom<number | null>(null);
--- src/components/pages/GameTimer.tsx
+++ src/components/pages/GameTimer.tsx
@@ -1,25 +1,16 @@
 import React, { useEffect } from 'react';
-import { atom, useAtom, useSetAtom } from 'jotai';
-import { gameScreenAtom } from '../../atoms/GameAtoms';
+import { useAtomValue, useSetAtom } from 'jotai';
+import { gameScreenAtom, gameTimeAtom } from '../../atoms/GameAtoms';
 import { GameScreen } from '../../types/game';
 
-// 게임 시간을 위한 atom
-export const gameTimeAtom = atom(1800); // 3분
-
 export const GameTimer = () => {
-  const [timeLeft, setTimeLeft] = useAtom(gameTimeAtom);
+  const timeLeft = useAtomValue(gameTimeAtom);
   const setGameScreen = useSetAtom(gameScreenAtom);
 
   useEffect(() => {
-    if (timeLeft <= 0) {
+    if (timeLeft !== null && timeLeft <= 0) {
       setGameScreen(GameScreen.GAME_OVER);
     }
-
-    const timer = setInterval(() => {
-      setTimeLeft((prev) => prev - 1);
-    }, 1000);
-
-    return () => clearInterval(timer);
   }, [timeLeft]);
 
   const formatTime = (seconds: number) => {
@@ -31,9 +22,11 @@ export const GameTimer = () => {
   return (
     <div className="absolute top-4 pl-10 z-50">
       <div className="bg-black/50 backdrop-blur-sm rounded-lg px-6 py-2">
-        <div className="text-white font-bold text-2xl">
-          {formatTime(timeLeft)}
-        </div>
+        {timeLeft && (
+          <div className="text-white font-bold text-2xl">
+            {formatTime(timeLeft)}
+          </div>
+        )}
       </div>
     </div>
   );
