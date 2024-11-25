5181f770f8c89ee2c159f035b55ef4375c5748b0
Soojin Lee <asyncwaiter@gmail.com>
2024-11-25 09:31:11 +0900

    ✨: [CD-105] error handler 추가

--- src/hooks/useApiError.tsx
+++ src/hooks/useApiError.tsx
@@ -1,11 +1,20 @@
-import React from 'react'
+import { useCallback } from 'react';
+import { toast } from 'react-toastify';
+type HttpStatus = number;
 
+const defaultHandler = (httpMessage: string = '에러가 발생했어요🥲') => {
+  toast.error(httpMessage);
+};
+
+const handlers: Record<HttpStatus | string, (msg: string) => void> = {
+  default: defaultHandler,
+};
 const useApiError = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+  const handleError = useCallback((error: Error) => {
+    const httpMessage: string = error.message;
+    handlers.default(httpMessage);
+  }, []);
+  return { handleError };
+};
 
-export default useApiError
+export default useApiError;
