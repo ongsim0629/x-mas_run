4c8b4403f114f674e388a84e4a217a37cd4a0b69
Soojin Lee <asyncwaiter@gmail.com>
2024-11-27 11:25:43 +0900

    :children_crossing:: Keyboard 가이드 수정

--- src/components/UI/KeyboardGuide.tsx
+++ src/components/UI/KeyboardGuide.tsx
@@ -1,6 +1,75 @@
 import { useEffect, useState } from 'react';
 import cls from 'classnames';
 
+type KeyboardMapProps = {
+  getKeyClass: (key: string) => string;
+  keyMap: string[];
+  movement: string[];
+  steal: string;
+  skill: string;
+};
+const KeyboardMap = ({
+  getKeyClass,
+  keyMap,
+  movement,
+  steal,
+  skill,
+}: KeyboardMapProps) => {
+  return (
+    <div className="w-full flex justify-center gap-20 items-end">
+      <div className="flex flex-col items-center gap-2">
+        <div className="flex gap-8 text-white">
+          <div className="flex flex-col gap-1">
+            <div className="flex justify-center">
+              <div className={cls(`w-10 h-10 ${getKeyClass(keyMap[0])}`)}>
+                {movement[0]}
+              </div>
+            </div>
+            <div className="flex gap-1">
+              <div className={cls(`w-10 h-10 ${getKeyClass(keyMap[1])}`)}>
+                {movement[1]}
+              </div>
+              <div className={cls(`w-10 h-10 ${getKeyClass(keyMap[2])}`)}>
+                {movement[2]}
+              </div>
+              <div className={cls(`w-10 h-10 ${getKeyClass(keyMap[3])}`)}>
+                {movement[3]}
+              </div>
+            </div>
+          </div>
+        </div>
+        <span className="text-sm text-white/70">이동하기</span>
+      </div>
+
+      <div className="flex gap-10 items-end text-white">
+        <div className="flex flex-col items-center gap-2">
+          <div className={cls(`w-32 h-10 ${getKeyClass('Space')}`)}>Space</div>
+          <span className="text-sm text-white/70">점프하기</span>
+        </div>
+      </div>
+      <div className="flex flex-col justify-center items-center gap-2">
+        <div className="flex gap-2">
+          <div className={cls(`w-20 h-10 ${getKeyClass(keyMap[4])}`)}>
+            {steal[0] === 'Mouse' ? '🖱️' : steal}
+          </div>
+          <div className="flex items-center text-2xl text-white/50 mx-2">
+            or
+          </div>
+
+          <div className={cls(`w-16 h-10 ${getKeyClass('KeyE')}`)}>E</div>
+        </div>
+        <span className="text-sm text-white/70">선물 훔치기</span>
+      </div>
+      <div className="flex flex-col justify-center items-center gap-2">
+        <div className={cls(`w-16 h-10 ${getKeyClass(keyMap[5])}`)}>
+          {skill}
+        </div>
+        <span className="text-sm text-white/70">스킬</span>
+      </div>
+    </div>
+  );
+};
+
 const KeyboardGuide = () => {
   const [pressedKeys, setPressedKeys] = useState<Set<string>>(new Set());
   const [isMouseDown, setIsMouseDown] = useState(false);
@@ -48,71 +117,31 @@ const KeyboardGuide = () => {
 
   return (
     <div
-      className={cls(`
-        absolute bottom-20 left-1/2 -translate-x-1/2
-        flex items-end gap-12
-      `)}
+      className="absolute bottom-20 left-1/2 -translate-x-1/2 flex flex-col gap-10 w-full"
       aria-label="key-control-guide"
     >
-      <div className={cls(`flex flex-col items-center gap-2`)}>
-        <div className={cls(`flex gap-8 text-white`)}>
-          <div className={cls(`flex flex-col gap-1`)}>
-            <div className={cls(`flex justify-center`)}>
-              <div className={cls(`w-10 h-10 ${getKeyClass('KeyW')}`)}>W</div>
-            </div>
-            <div className={cls(`flex gap-1`)}>
-              <div className={cls(`w-10 h-10 ${getKeyClass('KeyA')}`)}>A</div>
-              <div className={cls(`w-10 h-10 ${getKeyClass('KeyS')}`)}>S</div>
-              <div className={cls(`w-10 h-10 ${getKeyClass('KeyD')}`)}>D</div>
-            </div>
-          </div>
-          <div className={cls(`flex items-center text-2xl text-white/50`)}>
-            or
-          </div>
-          <div className={cls(`flex flex-col gap-1`)}>
-            <div className={cls(`flex justify-center`)}>
-              <div className={cls(`w-10 h-10 ${getKeyClass('ArrowUp')}`)}>
-                ↑
-              </div>
-            </div>
-            <div className={cls(`flex gap-1`)}>
-              <div className={cls(`w-10 h-10 ${getKeyClass('ArrowLeft')}`)}>
-                ←
-              </div>
-              <div className={cls(`w-10 h-10 ${getKeyClass('ArrowDown')}`)}>
-                ↓
-              </div>
-              <div className={cls(`w-10 h-10 ${getKeyClass('ArrowRight')}`)}>
-                →
-              </div>
-            </div>
-          </div>
-        </div>
-        <span className={cls(`text-sm text-white/70`)}>이동하기</span>
-      </div>
-
-      <div className={cls(`flex gap-10 items-end text-white`)}>
-        <div className={cls(`flex flex-col items-center gap-2`)}>
-          <div className={cls(`w-32 h-10 ${getKeyClass('Space')}`)}>Space</div>
-          <span className={cls(`text-sm text-white/70`)}>점프하기</span>
-        </div>
-      </div>
-      <div className={cls(`flex flex-col justify-center items-center gap-2`)}>
-        <div className={cls(`flex gap-2`)}>
-          <div className={cls(`w-16 h-10 ${getKeyClass('ShiftLeft')}`)}>
-            Shift
-          </div>
-          <div className={cls(`flex items-center text-2xl text-white/50 mx-2`)}>
-            or
-          </div>
-          <div className={cls(`w-10 h-10 ${getKeyClass('KeyE')}`)}>E</div>
-          <div className={cls(`flex items-center text-2xl text-white/50 mx-2`)}>
-            or
-          </div>
-          <div className={cls(`w-10 h-10 ${getKeyClass('Mouse')}`)}>🖱️</div>
-        </div>
-        <span className={cls(`text-sm text-white/70`)}>선물 훔치기</span>
-      </div>
+      <KeyboardMap
+        getKeyClass={getKeyClass}
+        keyMap={['KeyW', 'KeyA', 'KeyS', 'KeyD', 'Mouse', 'KeyQ']}
+        movement={['W', 'A', 'S', 'D']}
+        steal="Mouse"
+        skill="Q"
+      />
+      <KeyboardMap
+        getKeyClass={getKeyClass}
+        keyMap={[
+          'ArrowUp',
+          'ArrowLeft',
+          'ArrowDown',
+          'ArrowRight',
+          'ShiftLeft',
+          'ControlLeft',
+          'KeyE',
+        ]}
+        movement={['↑', '←', '↓', '→']}
+        steal="Shift"
+        skill="Ctrl"
+      />
     </div>
   );
 };
